094f06234338add954d2b651fd42795564e1a49c
Christian <chris.moeller-bosse@gmx.de>
2022-02-22 14:26:44 +0100

    Aufgabe 5

--- aufgaben/autos/AufgabeCollections.java
+++ aufgaben/autos/AufgabeCollections.java
@@ -1,11 +1,46 @@
 package aufgaben.autos;
+import java.util.*;
 
 public class AufgabeCollections {
     public static void main(String[] args) {
-        VW vw = new VW("Golf", 1990);
-        BMW bmw = new BMW("Z4", 2000);
+        VW vw1 = new VW("Golf", 1990);
+        VW vw2 = new VW("Polo", 2001);
+        VW vw3 = new VW("Passat", 2020);
+        BMW bmw1 = new BMW("Z4", 2000);
+        BMW bmw2 = new BMW("Modell", 2002);
 
-        System.err.println(vw);
-        System.out.println(bmw);
+        // System.err.println(vw1);
+        // System.out.println(bmw);
+
+        LinkedList linkList = new LinkedList();
+        HashSet hSet = new HashSet();
+        TreeSet tSet = new TreeSet();
+
+        linkList.add(vw1);
+        linkList.add(vw2);
+        linkList.add(vw3);
+
+        hSet.add(vw1);
+        hSet.add(vw2);
+        hSet.add(vw3);
+
+        tSet.add(vw1);
+        tSet.add(vw2);
+        tSet.add(vw3);
+
+        for (Object object : tSet) {
+            System.out.println(object);
+        }
+        System.err.println("------------------------------------");
+
+        for (Object object : hSet) {
+            System.out.println(object);            
+        }
+
+        System.err.println("------------------------------------");
+
+        for (Object object : linkList) {
+            System.out.println(object);            
+        }
     }
 }
--- aufgaben/autos/Auto.java
+++ aufgaben/autos/Auto.java
@@ -1,18 +1,69 @@
 package aufgaben.autos;
 
-public abstract class Auto {
-    protected int baujahr;
-    protected String modell;
+public abstract class Auto implements Comparable {
 
-    public Auto(String modell, int baujahr) {
-        this.modell = modell;
-        this.baujahr = baujahr;
+  protected int baujahr;
+  protected String modell;
+
+  public Auto(String modell, int baujahr) {
+    this.modell = modell;
+    this.baujahr = baujahr;
+  }
+
+  @Override
+  public int compareTo(Object o) {
+    if (this.modell.compareTo(((Auto)o).getModell())==0) {
+        if (this.baujahr>((Auto)o).getBaujahr()) {
+            return 1;
+        } else if (this.baujahr<((Auto)o).getBaujahr()) {
+            return -1;
+        } else {
+            return 0;
+        }
     }
+    return (this.modell.compareTo(((Auto)o).getModell()));
+  }
 
-    @Override
-    public String toString() {
-        return "Auto [baujahr=" + baujahr + ", modell=" + modell + "]";
-    }
+  @Override
+  public String toString() {
+    return "Auto [baujahr=" + baujahr + ", modell=" + modell + "]";
+  }
 
-    
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + baujahr;
+    result = prime * result + ((modell == null) ? 0 : modell.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) return true;
+    if (obj == null) return false;
+    if (getClass() != obj.getClass()) return false;
+    Auto other = (Auto) obj;
+    if (baujahr != other.baujahr) return false;
+    if (modell == null) {
+      if (other.modell != null) return false;
+    } else if (!modell.equals(other.modell)) return false;
+    return true;
+  }
+
+  public int getBaujahr() {
+    return baujahr;
+  }
+
+  public void setBaujahr(int baujahr) {
+    this.baujahr = baujahr;
+  }
+
+  public String getModell() {
+    return modell;
+  }
+
+  public void setModell(String modell) {
+    this.modell = modell;
+  }
 }
